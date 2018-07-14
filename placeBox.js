419e9871b65597a394fca4473fceddc0f2989625
Kwang <kwangthirakul@gmail.com>
2018-07-15 00:54:54 +0700

    15-jul-18 00:54

--- placeBox.js
+++ placeBox.js
@@ -1,24 +1,24 @@
 function placeBox() {
-	
-		document.getElementById('placeBox').style.display = 'flex';
-	
 
-setTimeout(function(){
-	document.getElementById('closePlace').style.opacity = '0.9';
-	
-},100)
-	
-	setTimeout(function(){
+	document.getElementById('placeBox').style.display = 'flex';
+
+
+	setTimeout(function () {
+		document.getElementById('closePlace').style.opacity = '0.9';
+
+	}, 100)
+
+	setTimeout(function () {
 		document.getElementById('xPlace').style.display = 'block';
-	},300)
-	
-	setTimeout(function(){
+	}, 300)
+
+	setTimeout(function () {
 		document.getElementById('placeIn').style.transform = 'scale(1)';
-	},100)
-	
+	}, 100)
 
 
-	
+
+
 	document.documentElement.style.overflow = 'hidden'; // firefox, chrome
 	document.body.scroll = "no"; // ie only
 
@@ -29,11 +29,91 @@ function xPlace() {
 	setTimeout(function () {
 		document.getElementById('placeBox').style.display = 'none';
 	}, 300);
-	
-	  document.getElementById('placeIn').style.transform = 'scale(0)';
 
-		document.getElementById('closePlace').style.opacity = '0';
+	document.getElementById('placeIn').style.transform = 'scale(0)';
+
+	document.getElementById('closePlace').style.opacity = '0';
 	document.documentElement.style.overflow = 'auto'; // firefox, chrome
 	document.body.scroll = "yes"; // ie only
 }
 
+function locaMB() {
+	document.getElementById('locaMenuTop').style.color = 'white';
+	document.getElementById('locaMenuTop').style.textShadow = '0px 0px 12px';
+
+	document.getElementById('eventMenuTop').style.color = '#b5b5b5';
+	document.getElementById('eventMenuTop').style.textShadow = 'none';
+
+	document.getElementById('invitMenuTop').style.color = '#b5b5b5';
+	document.getElementById('invitMenuTop').style.textShadow = 'none';
+
+	document.getElementById('analyMenuTop').style.color = '#b5b5b5';
+	document.getElementById('analyMenuTop').style.textShadow = 'none';
+
+
+	document.getElementById('LocaSpace').style.display = 'flex';
+	document.getElementById('EventSpace').style.display = 'none';
+	document.getElementById('inviSpace').style.display = 'none';
+	document.getElementById('analySpace').style.display = 'none';
+}
+
+function eventMB() {
+	document.getElementById('locaMenuTop').style.color = '#b5b5b5';
+	document.getElementById('locaMenuTop').style.textShadow = 'none';
+
+	document.getElementById('eventMenuTop').style.color = 'white';
+	document.getElementById('eventMenuTop').style.textShadow = '0px 0px 12px';
+
+	document.getElementById('invitMenuTop').style.color = '#b5b5b5';
+	document.getElementById('invitMenuTop').style.textShadow = 'none';
+
+	document.getElementById('analyMenuTop').style.color = '#b5b5b5';
+	document.getElementById('analyMenuTop').style.textShadow = 'none';
+
+
+	document.getElementById('LocaSpace').style.display = 'none';
+	document.getElementById('EventSpace').style.display = 'flex';
+	document.getElementById('inviSpace').style.display = 'none';
+	document.getElementById('analySpace').style.display = 'none';
+}
+
+
+function invitMB() {
+	document.getElementById('locaMenuTop').style.color = '#b5b5b5';
+	document.getElementById('locaMenuTop').style.textShadow = 'none';
+
+	document.getElementById('eventMenuTop').style.color = '#b5b5b5';
+	document.getElementById('eventMenuTop').style.textShadow = 'none';
+
+	document.getElementById('invitMenuTop').style.color = 'white';
+	document.getElementById('invitMenuTop').style.textShadow = '0px 0px 12px';
+
+	document.getElementById('analyMenuTop').style.color = '#b5b5b5';
+	document.getElementById('analyMenuTop').style.textShadow = 'none';
+
+
+	document.getElementById('LocaSpace').style.display = 'none';
+	document.getElementById('EventSpace').style.display = 'none';
+	document.getElementById('inviSpace').style.display = 'flex';
+	document.getElementById('analySpace').style.display = 'none';
+}
+
+function analyMB() {
+	document.getElementById('locaMenuTop').style.color = '#b5b5b5';
+	document.getElementById('locaMenuTop').style.textShadow = 'none';
+
+	document.getElementById('eventMenuTop').style.color = '#b5b5b5';
+	document.getElementById('eventMenuTop').style.textShadow = 'none';
+
+	document.getElementById('invitMenuTop').style.color = '#b5b5b5';
+	document.getElementById('invitMenuTop').style.textShadow = 'none';
+
+	document.getElementById('analyMenuTop').style.color = 'white';
+	document.getElementById('analyMenuTop').style.textShadow = '0px 0px 12px';
+
+
+	document.getElementById('LocaSpace').style.display = 'none';
+	document.getElementById('EventSpace').style.display = 'none';
+	document.getElementById('inviSpace').style.display = 'none';
+	document.getElementById('analySpace').style.display = 'flex';
+}
